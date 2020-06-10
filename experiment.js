9bb783024cb24e4c3aae4d3de111a3f36d9078ad
Jina Zhu <jinazhu87@gmail.com>
2020-06-10 11:43:02 -0700

    added scrollmagic

--- experiment.js
+++ experiment.js
@@ -13,18 +13,28 @@ const slide = document.querySelector(".hike");
 // }
 
 //triger an element when it comes into view: intersection observer
-let options = {
-  threshold: 0.5,
-};
-let observer = new IntersectionObserver(slideAnim, options);
+// let options = {
+//   threshold: 0.5,
+// };
+// let observer = new IntersectionObserver(slideAnim, options);
 
-function slideAnim(entries) {
-  // entries is all the item we have
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      slide.style.background = "white";
-    }
-  });
-}
-// these are entries, can have as many as you want
-observer.observe(slide);
+// function slideAnim(entries) {
+//   // entries is all the item we have
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       slide.style.background = "white";
+//     }
+//   });
+// }
+// // these are entries, can have as many as you want
+// observer.observe(slide);
+
+const controller = new ScrollMagic.Controller();
+
+const exploreScene = new ScrollMagic.Scene({
+  triggerElement: ".hike-exp",
+  triggerHook: 0.5,
+})
+  .addIndicators({ colorStart: "white", colorTrigger: "white" })
+  .setClassToggle(".hike-exp", "active")
+  .addTo(controller);
