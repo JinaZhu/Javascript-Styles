63665b8f0a7e875afef719dfcf96ee9c1714c3fa
Jina Zhu <jinazhu87@gmail.com>
2020-06-11 10:04:49 -0700

    added gsap slider to image

---
+++ app.js
@@ -0,0 +1,28 @@
+let controller;
+let slideScene;
+
+function animateSlides() {
+  // init controller
+  constroller = new ScrollMagic.Controller();
+  // select some things
+  const sliders = document.querySelectorAll(".slide");
+  const nav = document.querySelector(".nav-header");
+
+  sliders.forEach((slide) => {
+    // targeting the div that's hiding the image
+    const revealImg = slide.querySelector(".reveal-img");
+    const img = slide.querySelector("img");
+    const revealText = slide.querySelector(".reveal-text");
+    // GSAP (item, time, object with property I want to change)
+    // gsap.to(revealImg, 1, { x: "100%" });
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
+    // target item, object where to animate from, object where to animate to
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    // add overflow hidden to make the image zoom in and out instead of making it bigger
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 });
+  });
+}
+
+animateSlides();
